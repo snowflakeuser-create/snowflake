9e27b7d139e19331aee516b5556b7eafa4f49ae3
snowflakeuser-create <onkar-dipak.shivdavkar@capgemini.com>
2026-02-04 19:29:46 +0530

    Updated table name

--- Employee_DDL.sql
+++ Employee_DDL.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "SNOW_PRACTS"."PRACTICE_SCH"."Employee" ( EmployeeID NUMBER(38, 0) , FirstName VARCHAR , LastName VARCHAR , Email VARCHAR , PhoneNumber NUMBER(38, 0) , Department VARCHAR , Designation VARCHAR , JoinDate DATE , Salary NUMBER(38, 0) , Location VARCHAR ); 
+CREATE TABLE "SNOW_PRACTS"."PRACTICE_SCH"."Emp" ( EmployeeID NUMBER(38, 0) , FirstName VARCHAR , LastName VARCHAR , Email VARCHAR , PhoneNumber NUMBER(38, 0) , Department VARCHAR , Designation VARCHAR , JoinDate DATE , Salary NUMBER(38, 0) , Location VARCHAR ); 
 
 CREATE TEMP FILE FORMAT "SNOW_PRACTS"."PRACTICE_SCH"."temp_file_format_2025-07-15T11:58:23.597Z"
 	TYPE=CSV
@@ -11,10 +11,11 @@ CREATE TEMP FILE FORMAT "SNOW_PRACTS"."PRACTICE_SCH"."temp_file_format_2025-07-1
     TIME_FORMAT=AUTO
     TIMESTAMP_FORMAT=AUTO; 
 
-COPY INTO "SNOW_PRACTS"."PRACTICE_SCH"."Employee" 
+COPY INTO "SNOW_PRACTS"."PRACTICE_SCH"."Emp" 
 FROM (SELECT $1, $2, $3, $4, $5, $6, $7, $8, $9, $10
 	FROM '@"SNOW_PRACTS"."PRACTICE_SCH"."__snowflake_temp_import_files__"') 
 FILES = ('2025-07-15T11:57:54.186Z/Employee.csv') 
 FILE_FORMAT = '"SNOW_PRACTS"."PRACTICE_SCH"."temp_file_format_2025-07-15T11:58:23.597Z"' 
 ON_ERROR=ABORT_STATEMENT 
+
 -- For more details, see: https://docs.snowflake.com/en/sql-reference/sql/copy-into-table
